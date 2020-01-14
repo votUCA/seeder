78a830cb381411c6343e95b0541936bc64508e68
KROSF <rodrigosanabria22@gmail.com>
2020-01-14 19:03:11 +0100

    feat: added colegiate bodies

--- request/graphql-request.ts
+++ request/graphql-request.ts
@@ -2,11 +2,11 @@ import { GraphQLClient, request } from 'graphql-request'
 import faker = require('faker/locale/en_US')
 const argv = require('yargs').argv
 
-function randombetween (min: number, max: number) {
+function randombetween(min: number, max: number) {
   return Math.floor(Math.random() * (max - min + 1) + min)
 }
 
-function generate (max: number, thecount: number) {
+function generate(max: number, thecount: number) {
   const r = []
   let currsum = 0
   for (let i = 0; i < thecount - 1; i++) {
@@ -50,10 +50,11 @@ const createUser = /* GraphQL */ `
   }
 `
 
-const createColegiateBody = /* GraphQL */ `
-  mutation createColegiateBody($input: ColegiateBody!) {
-    createColegiateBody(input: $input) {
+const colegiateBodyQuery = /* GraphQL */ `
+  query collegiateBodies {
+    collegiateBodies {
       id
+      name
     }
   }
 `
@@ -65,15 +66,33 @@ const usersQuery = /* GraphQL */ `
       firstName
       lastName
       dni
+      colegiateBody {
+        id
+        name
+      }
     }
   }
 `
 
+function groupBy(list, keyGetter) {
+  const map = new Map()
+  list.forEach(item => {
+    const key = keyGetter(item)
+    const collection = map.get(key)
+    if (!collection) {
+      map.set(key, [item])
+    } else {
+      collection.push(item)
+    }
+  })
+  return map
+}
+
 const groups = ['PDVP', 'PNDVP', 'PDINVP', 'PAS', 'ALU']
 const locations = ['JEREZ', 'PUERTO REAL', 'CASEM', 'CADIZ', 'ALGECIRAS']
 
-async function main () {
-  const endpoint = 'http://votuca.krosf.com:7000/graphql'
+async function main() {
+  const endpoint = 'http://localhost:7000/graphql'
 
   const { login } = await request(endpoint, loginQuery, {
     input: argv.input
@@ -88,6 +107,8 @@ async function main () {
   })
 
   if (argv.users) {
+    const { collegiateBodies } = await graphQLClient.request(colegiateBodyQuery)
+
     const baseuid = 20192021
     for (let i = 0; i < 100; i++) {
       await graphQLClient.request(createUser, {
@@ -98,44 +119,37 @@ async function main () {
           firstName: faker.name.findName(),
           lastName: faker.name.lastName(),
           roles: 'VOTER',
+          colegiateBody: faker.random.arrayElement<any>(collegiateBodies).id,
           genre: faker.random.arrayElement(['MALE', 'FEMALE', 'OTHER'])
         }
       })
     }
   }
 
-  if (argv.body) {
-    for (let i = 0; i < groups.length; ++i) {
-      await graphQLClient.request(createColegiateBody, {
-        input: {
-          name: groups[i]
-        }
-      })
-    }
-  }
-
   const users = await graphQLClient.request(usersQuery)
+  const grouped = groupBy(users.users, user => user.colegiateBody.name)
 
   if (argv.polls) {
-    for (let i = 0; i < 1; i++) {
+    for (let i = 0; i < 10; i++) {
       const result = await graphQLClient.request(createPoll, {
         input: {
           description: faker.lorem.paragraph(1),
           start: new Date().toISOString(),
           end: new Date('01/20/2020').toISOString(),
           delegates: [],
-          censuses: locations
-            .map(location =>
-              groups.map(group => ({
-                group,
-                location,
-                date: new Date().toISOString(),
-                voters: new Array(faker.random.number(10) + 1)
-                  .fill(null)
-                  .map(() => faker.random.arrayElement(users.users))
-              }))
-            )
-            .reduce((prev, current) => [...prev, ...current]),
+          censuses: groups.map(group => ({
+            group,
+            location: 'ESI',
+            date: new Date().toISOString(),
+            voters: new Array(faker.random.number(10) + 1)
+              .fill(null)
+              .map(() => {
+                const { colegiateBody, ...voter } = faker.random.arrayElement<
+                  any
+                >(grouped.get(group))
+                return voter
+              })
+          })),
           question: faker.lorem.sentence(10),
           options: new Array(faker.random.number(5) + 2)
             .fill(null)
@@ -148,7 +162,7 @@ async function main () {
   }
 
   if (argv.elections) {
-    for (let i = 0; i < 1; i++) {
+    for (let i = 0; i < 10; i++) {
       const numCandidates = faker.random.number(5) + 1
       const result = await graphQLClient.request(createElection, {
         input: {
@@ -163,18 +177,19 @@ async function main () {
               lastName: faker.name.lastName(),
               about: faker.lorem.paragraph(10)
             })),
-          censuses: locations
-            .map(location =>
-              groups.map(group => ({
-                group,
-                location,
-                date: new Date().toISOString(),
-                voters: new Array(faker.random.number(10) + 1)
-                  .fill(null)
-                  .map(() => faker.random.arrayElement(users.users))
-              }))
-            )
-            .reduce((prev, current) => [...prev, ...current]),
+          censuses: groups.map(group => ({
+            group,
+            location: 'ESI',
+            date: new Date().toISOString(),
+            voters: new Array(faker.random.number(10) + 1)
+              .fill(null)
+              .map(() => {
+                const { colegiateBody, ...voter } = faker.random.arrayElement<
+                  any
+                >(grouped.get(group))
+                return voter
+              })
+          })),
           maxVotes: faker.random.number(numCandidates) + 1,
           voteWeights: generate(100, groups.length).map((weight, index) => ({
             weight,
