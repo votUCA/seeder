334cae071ebc7e34654168ae846e59bc0e3eaf1a
KROSF <rodrigosanabria22@gmail.com>
2020-01-14 14:00:23 +0100

    feat: added parser argv

--- request/graphql-request.ts
+++ request/graphql-request.ts
@@ -1,5 +1,6 @@
 import { GraphQLClient, request } from 'graphql-request'
 import faker = require('faker/locale/en_US')
+const argv = require('yargs').argv
 
 function randombetween (min: number, max: number) {
   return Math.floor(Math.random() * (max - min + 1) + min)
@@ -16,22 +17,66 @@ function generate (max: number, thecount: number) {
   return r
 }
 
-async function main () {
-  const endpoint = 'http://localhost:9998/graphql'
-
-  const loginQuery = /* GraphQL */ `
-    mutation login($input: LoginInput!) {
-      login(input: $input) {
-        accessToken
-      }
+const loginQuery = /* GraphQL */ `
+  mutation login($input: LoginInput!) {
+    login(input: $input) {
+      accessToken
     }
-  `
+  }
+`
+
+const createElection = /* GraphQL */ `
+  mutation election($input: ElectionInput!) {
+    createElection(input: $input) {
+      start
+      end
+    }
+  }
+`
+
+const createPoll = /* GraphQL */ `
+  mutation poll($input: PollInput!) {
+    createPoll(input: $input) {
+      id
+    }
+  }
+`
+
+const createUser = /* GraphQL */ `
+  mutation createUser($input: UserInput!) {
+    createUser(input: $input) {
+      id
+    }
+  }
+`
+
+const createColegiateBody = /* GraphQL */ `
+  mutation createColegiateBody($input: ColegiateBody!) {
+    createColegiateBody(input: $input) {
+      id
+    }
+  }
+`
+
+const usersQuery = /* GraphQL */ `
+  query users {
+    users {
+      uid
+      firstName
+      lastName
+      dni
+    }
+  }
+`
+
+const groups = ['PDVP', 'PNDVP', 'PDINVP', 'PAS', 'ALU']
+const locations = ['JEREZ', 'PUERTO REAL', 'CASEM', 'CADIZ', 'ALGECIRAS']
+
+async function main () {
+  const endpoint = 'http://votuca.krosf.com:7000/graphql'
 
   const { login } = await request(endpoint, loginQuery, {
-    input: {
-      uid: 'u20192020',
-      password: 'votUCA?0!9'
-    }
+    input: argv.input
   })
 
   const headers = {
@@ -42,31 +87,7 @@ async function main () {
     headers
   })
 
-  const pedingElections = /* GraphQL */ `
-    mutation election($input: ElectionInput!) {
-      createElection(input: $input) {
-        start
-        end
-      }
-    }
-  `
-  const pedingPolls = /* GraphQL */ `
-    mutation poll($input: PollInput!) {
-      createPoll(input: $input) {
-        start
-        end
-      }
-    }
-  `
-  const createUser = /* GraphQL */ `
-    mutation createUser($input: UserInput!) {
-      createUser(input: $input) {
-        id
-      }
-    }
-  `
-  const createUserboolean = false
-  if (createUserboolean) {
+  if (argv.users) {
     const baseuid = 20192021
     for (let i = 0; i < 100; i++) {
       await graphQLClient.request(createUser, {
@@ -83,24 +104,21 @@ async function main () {
     }
   }
 
-  const groups = ['PDVP', 'PNDVP', 'PDINVP', 'PAS', 'ALU']
-  const locations = ['JEREZ', 'PUERTOREAL', 'CASEM', 'CADIZ', 'ALGECIRAS']
-  const usersQuery = /* GraphQL */ `
-    query users {
-      users {
-        uid
-        firstName
-        lastName
-        dni
-      }
+  if (argv.body) {
+    for (let i = 0; i < groups.length; ++i) {
+      await graphQLClient.request(createColegiateBody, {
+        input: {
+          name: groups[i]
+        }
+      })
     }
-  `
+  }
+
   const users = await graphQLClient.request(usersQuery)
 
-  const createPollBoolean = true
-  if (createPollBoolean) {
+  if (argv.polls) {
     for (let i = 0; i < 1; i++) {
-      const result = await graphQLClient.request(pedingPolls, {
+      const result = await graphQLClient.request(createPoll, {
         input: {
           description: faker.lorem.paragraph(1),
           start: new Date().toISOString(),
@@ -129,11 +147,10 @@ async function main () {
     }
   }
 
-  const createElectionBoolean = true
-  if (createElectionBoolean) {
+  if (argv.elections) {
     for (let i = 0; i < 1; i++) {
       const numCandidates = faker.random.number(5) + 1
-      const result = await graphQLClient.request(pedingElections, {
+      const result = await graphQLClient.request(createElection, {
         input: {
           description: faker.lorem.paragraph(1),
           start: new Date().toISOString(),
